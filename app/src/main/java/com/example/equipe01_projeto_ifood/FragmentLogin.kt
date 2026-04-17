9228bcf4e79caa94ccad96f240d933c036030471
Thiago Castagnoli <murilothiago813@gmail.com>
2026-04-17 17:22:16 -0300

    feat: Tela de Login

---
+++ app/src/main/java/com/example/equipe01_projeto_ifood/FragmentLogin.kt
@@ -0,0 +1,20 @@
+package com.example.equipe01_projeto_ifood
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.Fragment
+
+class FragmentLogin: Fragment(R.layout.login_fragment){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+
+
+}
+
+
