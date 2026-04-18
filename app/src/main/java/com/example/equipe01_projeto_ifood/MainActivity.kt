07a91b695bd6bc3a3e891925bad55007bfb63ac2
Raniel Schneider <ranielschneider@hotmail.com>
2026-04-18 22:51:34 +0100

    feat: adicioina tela de detalhes separada

--- app/src/main/java/com/example/equipe01_projeto_ifood/MainActivity.kt
+++ app/src/main/java/com/example/equipe01_projeto_ifood/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.equipe01_projeto_ifood
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,19 +9,15 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnBack: ImageButton
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        btnBack = findViewById(R.id.btn_back)
-
-        btnBack.setOnClickListener {
-            finish()
+        setContentView(R.layout.activity_detalhes_restaurante)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-
 
     }
 }
