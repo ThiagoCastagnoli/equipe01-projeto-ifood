4e654ffde20c17a7b7d7092a487c7ad208a1bf59
Raniel Schneider <ranielschneider@hotmail.com>
2026-04-18 22:14:46 +0100

    feat: tela + botão voltar

--- app/src/main/java/com/example/equipe01_projeto_ifood/MainActivity.kt
+++ app/src/main/java/com/example/equipe01_projeto_ifood/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.equipe01_projeto_ifood
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnBack: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnBack = findViewById(R.id.btn_back)
+
+        btnBack.setOnClickListener {
+            finish()
         }
+
+
     }
 }
