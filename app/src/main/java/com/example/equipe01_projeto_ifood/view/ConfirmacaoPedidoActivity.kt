b7114ef60a716beacef334dde649382f25d13313
MatheusCeolin <matheus_fer1@hotmail.com>
2026-04-26 20:58:32 -0300

    feat: implementar navegação da tela confirmação_pedido para a tela detalhes_pedido e para a tela detalhes_restaurantes
    style: correção do nome botão voltar

--- app/src/main/java/com/example/equipe01_projeto_ifood/view/ConfirmacaoPedidoActivity.kt
+++ app/src/main/java/com/example/equipe01_projeto_ifood/view/ConfirmacaoPedidoActivity.kt
@@ -1,13 +1,31 @@
 package com.example.equipe01_projeto_ifood.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.equipe01_projeto_ifood.DetalhesRestaurantes
 import com.example.equipe01_projeto_ifood.R
 
 class ConfirmacaoPedidoActivity : AppCompatActivity() {
+
+    private lateinit var btnVoltar: ImageButton
+    private lateinit var btnConfirmacaoPedidoFinal: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_confirmacao_pedido)
+
+        btnVoltar = findViewById(R.id.btn_voltar)
+        btnVoltar.setOnClickListener {
+            finish()
     }
+        val button = findViewById<Button>(R.id.btn_confirmar_pedido_final)
+
+        button.setOnClickListener {
+            val intent = Intent(this, DetalhesRestaurantes::class.java)
+            startActivity(intent)
+        }
+}
 }
